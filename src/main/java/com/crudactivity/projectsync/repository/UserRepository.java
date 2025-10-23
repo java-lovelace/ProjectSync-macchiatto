eeffeba899446be911341384c5c83e35c5b5c7bd
bany-ph <bpuertaherrera@gmai.com>
2025-10-23 13:48:38 -0500

    feat: create repository for user and project

--- src/main/java/com/crudactivity/projectsync/repository/UserRepository.java
+++ src/main/java/com/crudactivity/projectsync/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.crudactivity.projectsync.repository;
 
 import com.crudactivity.projectsync.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
 }
